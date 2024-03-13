fe56a69c57b2e93a0dcfdf51cde6b8dc71f99c91
Mauricio.M <m.morocho1196@gmail.com>
2024-03-12 22:45:26 -0500

    Adding lowercase standard to uniqueness validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -49,9 +49,16 @@ class UserTest < ActiveSupport::TestCase
 
   test "email addresses should be unique" do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
+    # duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "email address should be saved as lowercase" do
+    mixed_case_email = "FoO@ExAmPLe.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
 end
